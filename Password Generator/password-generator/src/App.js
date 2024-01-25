f39dbc5b8c4ff8b7883c264dc8477933731f92e5
Marcio Schlichting <marcioosergioo2015@gmail.com>
2024-01-25 14:42:40 -0300

    Creating this 'beautiful' page e.e

--- Password Generator/password-generator/src/App.js
+++ Password Generator/password-generator/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import generateStrongPassword from './utils/password-generator.ts';
+import copyToClipboard from './utils/clipboard.ts';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const PASSWORD_LENGTH = 30;
+    const [password, setPassword] = useState('test');
+    
+    const handleGeneratePassword = () => {
+        let strongPassword = generateStrongPassword(PASSWORD_LENGTH);
+        setPassword(strongPassword);
+    }
+    
+    const handleCopyToClipboard = async () => {
+        await copyToClipboard(password);
+    }
+    
+    return (
+        <div style={{
+            width: '100vw',
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                borderRadius: '3px',
+                width: '300px',
+                height: '200px',
+                backgroundColor: 'lightblue',
+                padding: '20px'
+            }}>
+                <button
+                    style={{
+                        width: '200px',
+                        height: '40px',
+                        marginBottom: '20px',
+                        cursor: 'pointer'
+                    }}
+                    onClick={handleGeneratePassword}
+                >
+                    Generate a strong password
+                </button>
+
+                <p style={{ wordWrap: 'break-word' }}>{password}</p>
+                
+                <button id='clipboardBtn' onClick={() => copyToClipboard(password)}
+                    style={{
+                    width: '200px',
+                    height: '40px',
+                    marginBottom: '20px',
+                    cursor: 'pointer'
+                }}>
+                    Copy to Clipboard
+                </button >
+            </div>
+        </div>
+    );
 }
 
 export default App;
